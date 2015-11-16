ea179edf0c56b749cc5fd2b076f2e261744d3d5d
Dezső BICZÓ <mxr576@gmail.com>
2015-11-16 19:56:12 +0100

    Minor logic fix, improve modules documentation.

--- index.js
+++ index.js
@@ -6,17 +6,37 @@ var util = require('util');
 var _ = require('underscore');
 var helper = require('./lib/helper');
 
+/**
+ * WebPage Content Extractor.
+ *
+ * @param {string[]} extractors - Name of the extractors in order.
+ * @param {string[]} options - Settings of the extractors.
+ *
+ * @constructor
+ */
 function WebPageContentExtractor(extractors, options) {
   this.extractor = extractorFactory.get(extractors, options);
   this.proxy = _.indexOf(extractors, 'wce-proxy') == '-1' ? null : extractorFactory.get(['wce-proxy'], options);
 }
 
+/**
+ * Extract the content of the url with the extractors.
+ *
+ * @param {string} url - Valid URL.
+ * @param {Object} extractor - Current Extractor object.
+ * @param {Object} emitter - EventEmitter object.
+ * @param {Object[]} errors - Array of error objects.
+ * @returns {Object}
+ */
 WebPageContentExtractor.prototype.extract = function (url, extractor, emitter, errors) {
   var self = this;
+  // Make sure that these variables are initialized.
   var emitter = emitter ? emitter : new EventEmitter();
   var error_log = errors ? errors : [];
+
   if (!extractor) {
     extractor = self.extractor;
+    // Validate the URL first.
     helper.isValidUrl(url, function (error) {
       if (error) {
         throw new Error(util.format('Invalid URL: "%s"', url));
@@ -24,8 +44,12 @@ WebPageContentExtractor.prototype.extract = function (url, extractor, emitter, e
     });
   }
 
+  // Call the current extractor's extract function.
   extractor.extract(url, function (error, result) {
     if (error) {
+      // If this extractor has fallback, call this function with fallback
+      // recursively.
+      // Pass the the current extractor's error too.
       if (extractor.fallback) {
         error_log.push(error);
         self.extract(url, extractor.fallback, emitter, error_log);
@@ -36,6 +60,7 @@ WebPageContentExtractor.prototype.extract = function (url, extractor, emitter, e
       }
     }
     else {
+      // Check, if WCE-Proxy in use. If yes. try to store the extracted content.
       if (self.proxy) {
         self.proxy.store(url, result.content, function (error) {
           if (error) {
